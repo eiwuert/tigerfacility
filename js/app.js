46fe4abd50087469da00f56b3d7435dcb32e7de3
softwaredeveloper04 <vladimiruzelac@Vladimirs-MacBook-Pro.local>
2019-09-16 19:28:51 +0200

    clean start

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('tigerfacility', ['ngAnimate', 'ngRoute', 'ui.bootstrap']);
+
+app.config(function($routeProvider,$locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+        .when("/", {
+            templateUrl : "pages/qualitaet.html"
+        })
+    .otherwise({ redirectTo: '/' });
+});
